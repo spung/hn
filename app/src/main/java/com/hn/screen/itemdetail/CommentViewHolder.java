ae348cf46ed2eddbbf6dff87b489e70588a979b7
Steven Pungdumri <steven@sprig.com>
2017-07-29 19:22:11 -0700

    - add background color spectrum for comment indentations

--- app/src/main/java/com/hn/screen/itemdetail/CommentViewHolder.java
+++ app/src/main/java/com/hn/screen/itemdetail/CommentViewHolder.java
@@ -3,6 +3,8 @@ package com.hn.screen.itemdetail;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.hn.R;
@@ -17,25 +19,25 @@ import butterknife.ButterKnife;
  */
 
 public class CommentViewHolder extends RecyclerView.ViewHolder {
+    private static int sIndentation;
     private Item mItem;
 
     @BindView(R.id.author) TextView mAuthorTextView;
     @BindView(R.id.text) TextView mTextTextView;
+    @BindView(R.id.comment) LinearLayout mCommentLinearLayout;
 
     public CommentViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
+        sIndentation = ResHelper.getDimen(itemView.getContext(), R.dimen.indentation_spacing);
     }
 
     public void bindView(Item item) {
         mItem = item;
         mAuthorTextView.setText(mItem.getBy());
         mTextTextView.setText(Html.fromHtml(item.getText()).toString().trim());
-        itemView.setPadding(
-            ResHelper.getDimen(itemView.getContext(), R.dimen.indentation_spacing) * item.getIndentation(),
-            itemView.getPaddingTop(),
-            itemView.getPaddingRight(),
-            itemView.getPaddingBottom()
-        );
+        ((FrameLayout.LayoutParams) mCommentLinearLayout.getLayoutParams()).leftMargin =
+            sIndentation * (item.getIndentation() - 1);
+
     }
 }
